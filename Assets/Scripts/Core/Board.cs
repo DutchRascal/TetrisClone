a459b711511aea1eac26930713a26d4138393884
DutchRascal <DutchRascal@fritz.box>
2020-02-23 14:19:59 +0100

    TC-014

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform m_emptySprite;
+    //public int m_height = 30;
+    public int m_height = 10;
+    public int m_width = 10;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        DrawEmptyCells();
+    }
+    void DrawEmptyCells()
+    {
+        for (int x = 0; x < m_width; x++)
+        {
+            for (int y = 0; y < m_height; y++)
+            {
+                Vector3 position = new Vector3(x - 3.5f, y - 10.4f, 0);
+                Instantiate(m_emptySprite, position, Quaternion.Euler(0, 0, 0));
+            }
+        }
     }
 }
