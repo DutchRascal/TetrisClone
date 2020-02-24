26bdc13eed25db6193382113e82955e63956fc6b
DutchRascal <DutchRascal@fritz.box>
2020-02-24 17:33:23 +0100

    TC-028

--- Assets/Scripts/Managers/GameController.cs
+++ Assets/Scripts/Managers/GameController.cs
@@ -10,7 +10,16 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        m_gameBoard = GameObject.FindObjectOfType<Board>();
+        m_spawner = GameObject.FindObjectOfType<Spawner>();
+        if (!m_gameBoard)
+        {
+            Debug.LogWarning("GAMECONTROLLER START: There is no game board defined!");
+        }
+        if (!m_spawner)
+        {
+            Debug.LogWarning("GAMECONTROLLER START: There is no spawner defined!");
+        }
     }
 
     // Update is called once per frame
