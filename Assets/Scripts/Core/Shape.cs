5599f115760f3461c65faf3b6a88b2553e95e5b9
DutchRascal <DutchRascal@fritz.box>
2020-02-24 16:34:42 +0100

    TC-026

--- Assets/Scripts/Core/Shape.cs
+++ Assets/Scripts/Core/Shape.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public bool m_canRotate = true;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Move(Vector3 moveDirection)
+    {
+        transform.position += moveDirection;
+    }
+
+    public void MoveLeft()
+    {
+        Move(new Vector3(-1, 0, 0));
+    }
+
+    public void MoveRight()
+    {
+        Move(new Vector3(1, 0, 0));
+    }
+
+    public void MoveDown()
+    {
+        Move(new Vector3(0, -1, 0));
+    }
+
+    public void MoveUp()
+    {
+        Move(new Vector3(0, 1, 0));
+    }
+
+    public void RotateRight()
+    {
+        if (m_canRotate)
+        {
+            transform.Rotate(0, 0, -90);
+        }
+    }
+
+    public void RotateLeft()
+    {
+        if (m_canRotate)
+        {
+            transform.Rotate(0, 0, 90);
+        }
     }
 }
