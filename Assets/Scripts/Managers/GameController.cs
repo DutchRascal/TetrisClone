b56607c8a58af0cda8d436a45744688b9e45824d
DutchRascal <DutchRascal@fritz.box>
2020-02-24 16:53:56 +0100

    TC-027

---
+++ Assets/Scripts/Managers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Board m_gameBoard;
+    Spawner m_spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
